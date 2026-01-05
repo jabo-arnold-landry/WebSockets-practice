c82a8f78fbb3ece57333426922be561f398be04b
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-05 20:16:21 +0200

    chore:Adding boiler plate to get started

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Title</h1>
+        <button>Add +</button>
+      </header>
+
+      <main>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Exercitationem nam dolorem, commodi debitis quis harum cupiditate
+          laboriosam necessitatibus rerum similique quibusdam vero dignissimos
+          numquam ea? Quaerat repellat porro nemo alias, praesentium officiis
+          blanditiis. Excepturi placeat ullam nihil quos! Veritatis, labore.
+        </p>
+        <div className="controls">
+          <button>comment</button>
+          <button>like</button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
