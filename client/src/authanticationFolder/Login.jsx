c9a64b03637532c1f1fdda474d374d316e90cf44
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-19 19:54:35 +0200

    feat:Add authantication and authorization forms interfaces

---
+++ client/src/authanticationFolder/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Login() {
+  return (
+    <>
+      <h1>Creat accound</h1>
+      <form action="">
+        <div className="input-group">
+          <label htmlFor="email">email</label>
+          <input type="text" id="email" placeholder="email goes here" />
+          <br />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">password</label>
+          <input type="password" id="password" placeholder="***" />
+          <br />
+        </div>
+        <button>Create</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
