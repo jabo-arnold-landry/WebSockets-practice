2c05004f22eeea076edbb9d5ca46d85240d84669
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-19 11:30:35 +0200

    feat:Add a verification file to verify user token when they request a certain resources to the server

---
+++ server/tokenVerification.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyingUser = (req, res, next) => {
+  const authHeaders = req.header.Authorzation || req.header.authorzation;
+  if (!authHeaders)
+    return res.status(403).json({ message: "Not allowed to attempt so" });
+  let token = authHeaders.split(" ")[1];
+  if (!token)
+    return res
+      .status(403)
+      .json({ message: "session expired try to login again" });
+  jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+    if (err) return res.status(401).json({ message: "invalid token" });
+    req.user = data;
+    next();
+  });
+};
+
+module.exports = verifyingUser;
