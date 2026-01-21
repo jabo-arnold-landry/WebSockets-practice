b435f2f42fa0cdb9c04b284580b4bb2fd82836f9
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-21 19:45:10 +0200

    feat:Add the wrapper for context for the authantication to the whole program

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import AuthProvider from "./contexts/useAuth.jsx";
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </AuthProvider>,
+);
