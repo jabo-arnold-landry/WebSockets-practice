7a7c382a179b773665e20b92b7cf2e92ea84bfd8
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-23 08:44:34 +0200

    fix:Fix the status code when the token has broken from 403(forbidden) to 401(unauthorized)

--- server/refreshRegeneration.js
+++ server/refreshRegeneration.js
@@ -6,7 +6,7 @@ const regenerateAccesToken = async (req, res, next) => {
   if (!token?.token) return res.status(403).json({ message: "not allowed" });
   jwt.verify(token.token, process.env.REFRESH_KEY, (err, data) => {
     if (err) {
-      return res.status(403).json({ message: "invalid refresh token" });
+      return res.status(401).json({ message: "invalid refresh token" });
     }
     const newAccessToken = jwt.sign(data, process.env.SECRET_KEY);
     res.status(200).json(newAccessToken);
