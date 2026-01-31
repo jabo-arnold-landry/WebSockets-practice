ae66303f6bf55a13c65e8ad180b71f01c312efc4
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-01-31 17:56:13 +0200

    fix:Change the structure of the user account on the site

--- server/authantication.js
+++ server/authantication.js
@@ -1,26 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { AccountCreation } = require("./model/siteModels");
+const { v4: uuid, v4 } = require("uuid");
 
 const users = [
   {
-    names: "jabo",
+    id: "7c30335f-b5eb-4975-92d6-7f477db6835e",
+    names: "arnold",
     email: "arnoldjabo@gmail.com",
+    postCreated: [],
+    postCommented: [],
   },
 ];
+
 //acc creation endpoints
 router.post("/signup", async (req, res) => {
   const userData = req.body;
+  const id = v4();
+
   if (!userData)
     return res.status(400).json({ message: "input the information please!" });
-  users.push(userData);
-  res.status(201).json({ message: "the created successfully" });
+
+  const findEmail = users.find((user) => user.email === userData.email);
+  if (findEmail)
+    return res
+      .status(400)
+      .json({ message: "The email exists in the community" });
+
+  const accInstance = new AccountCreation(id, userData.names, userData.email);
+  users.unshift(accInstance);
+  res.status(201).json({ message: "the user created successfully", users });
 });
 
 // acc verification endpoints
 router.post("/login", async (req, res) => {
   const userData = req.body;
   const foundUser = users.find((user) => user.email === userData.email);
+
   if (!foundUser)
     return res
       .status(404)
@@ -34,6 +51,7 @@ router.post("/login", async (req, res) => {
   const refreshToken = jwt.sign(foundUser, process.env.REFRESH_KEY, {
     expiresIn: "30d",
   });
+
   res.cookie("token", refreshToken, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
