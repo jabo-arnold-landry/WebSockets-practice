c7f2bd466d8ed021ec3016daac32320f82864fbb
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-14 19:30:46 +0200

    feat:Add a mock data to test for different users when working and testing with the site
    this will help to have different users and test the real-data communication and also make the chat applicable so I added some new users stastically for me to test
    because I don't have a database to persist data for now that's why

--- server/model/siteModels.js
+++ server/model/siteModels.js
@@ -27,5 +27,26 @@ const users = [
     postCreated: [],
     postCommented: [],
   },
+  {
+    id: "7c30335f-b5eb-4975-92d6-7f477db645t",
+    names: "landry",
+    email: "landry@gmail.com",
+    postCreated: [],
+    postCommented: [],
+  },
+  {
+    id: "7c30335f-b5eb-4975-92d6-7f477db7483e",
+    names: "jabo",
+    email: "jabo@gmail.com",
+    postCreated: [],
+    postCommented: [],
+  },
+  {
+    id: "7c30335f-b5eb-4975-92d6-7f477db8912e",
+    names: "ministry of education",
+    email: "ministryofecucation@gmail.com",
+    postCreated: [],
+    postCommented: [],
+  },
 ];
 module.exports = { AccountCreation, Idea, users };
