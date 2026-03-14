5acf997bd2587796011fe17e9bb0e818a8d4694d
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-14 19:57:38 +0200

    fix:Remove duplication return of the same property twice in the object we are sending from my own custom hook

--- client/src/hooks/useAuthHook.jsx
+++ client/src/hooks/useAuthHook.jsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../contexts/useAuth";
 
 function useAuthHook() {
   const { user, setUser, setUserData, userData } = useContext(AuthContext);
-  return { user, setUser, userData, setUser };
+  return { user, setUser, userData, setUserData };
 }
 
 export default useAuthHook;
