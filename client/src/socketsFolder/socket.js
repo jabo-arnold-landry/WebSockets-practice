375e1fd5ca511f9c3733a1941c08d5df83291c64
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-20 12:35:28 +0200

    feat:Add a namaspace endpoint for chatting features

--- client/src/socketsFolder/socket.js
+++ client/src/socketsFolder/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 const url = "http://localhost:3000";
 const socket = io(url);
+export const messageSocket = io(`${url}/messages`, { autoConnect: false });
 
 export default socket;
