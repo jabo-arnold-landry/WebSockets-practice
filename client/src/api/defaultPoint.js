d24077ab76e657adee10e8b088b3ca8a3ad3d55a
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-20 12:33:47 +0200

    feat:Export the BASEURL variable to be able to access to every file without needing to re-write the endpoint prefix everytime

--- client/src/api/defaultPoint.js
+++ client/src/api/defaultPoint.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL =  "http://localhost:3000"
+export const BASEURL =  "http://localhost:3000"
 const api = axios.create({
   baseURL: BASEURL,
   withCredentials: true,
