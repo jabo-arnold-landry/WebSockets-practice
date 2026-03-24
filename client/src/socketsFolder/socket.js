23a776e7bbdfaa37dfe9aed9ff7876161151ca78
jabo <arnoldjabo@gmail.com>
2026-03-24 20:18:45 +0200

    feat: Update socket connection to disable autoConnect

--- client/src/socketsFolder/socket.js
+++ client/src/socketsFolder/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const url = "http://localhost:3000";
-const socket = io(url);
+const socket = io(url, {autoConnect: false});
 export const messageSocket = io(`${url}/messages`, { autoConnect: false });
 
 export default socket;
