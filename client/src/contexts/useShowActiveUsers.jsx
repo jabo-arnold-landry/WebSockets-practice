c29bbb09c06f39dea22230f4743dbbe604130f93
jabo <arnoldjabo@gmail.com>
2026-03-26 19:54:02 +0200

    feat: Add ShowActiveUsers context for managing active user state

---
+++ client/src/contexts/useShowActiveUsers.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { createContext, useState } from "react";
+
+export const ActiveUsers = createContext();
+
+const ShowActiveUsers = ({ children }) => {
+  const [activeUsers, setActiveUsers] = useState([]);
+  return (
+    <>
+      <ActiveUsers.Provider value={{ activeUsers, setActiveUsers }}>
+        {children}
+      </ActiveUsers.Provider>
+    </>
+  );
+};
+
+
+
+
+export default ShowActiveUsers;
