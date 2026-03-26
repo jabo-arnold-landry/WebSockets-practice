4ab3bb20d0dbca29101f8b47bb204c3566342732
jabo <arnoldjabo@gmail.com>
2026-03-26 19:51:41 +0200

    feat: Implement MessageSection component for user messaging functionality

---
+++ client/src/MessageSection.jsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react";
+import { messageSocket } from "./socketsFolder/socket";
+import { BASEURL } from "./api/defaultPoint";
+import { useState } from "react";
+import { useActionState } from "react";
+import socket from "./socketsFolder/socket";
+import { useLocation } from "react-router-dom";
+
+function MessageSection() {
+  const [_, action, isPending] = useActionState(sendMessage, "");
+  const [recipientId, setRecipientId] = useState();
+  const location = useLocation();
+  console.log(location);
+
+  function sendMessage(_, formData) {
+    const chat = formData.get("message");
+    let obj = { recipientId, chat };
+
+    messageSocket.emit("message", obj);
+  }
+
+  useEffect(() => {
+    messageSocket.connect();
+    socket.on("allofus", (res) => {
+      console.log(res);
+    });
+
+    return () => {
+      messageSocket.off("message");
+      messageSocket.off("connect");
+    };
+  }, [recipientId]);
+  console.log(recipientId);
+  return (
+    <div>
+      <UserList setRecipientId={setRecipientId} />
+      <section className="discussion-section">
+        <p className="sender">sending love to you</p>
+        <p className="receiver">Thanks</p>
+      </section>
+      {recipientId && (
+        <section className="sending-section">
+          <form action={action}>
+            <input
+              type="text"
+              name="message"
+              id="message"
+              placeholder="message goes here"
+            />
+            <button disabled={isPending}>
+              {isPending ? " Sending..." : "Send"}
+            </button>
+          </form>
+        </section>
+      )}
+    </div>
+  );
+}
+
+function UserList({ setRecipientId }) {
+  const [users, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const abortController = new AbortController();
+    const userList = async () => {
+      try {
+        setIsLoading(true);
+        const serverResponse = await fetch(`${BASEURL}/myusers`, {
+          signal: abortController.signal,
+        });
+        const users = await serverResponse.json();
+        setUserList(users);
+      } catch (err) {
+        console.log("something bad happened: ", err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    userList();
+  }, []);
+  return (
+    <>
+      <p>{isLoading && "Loading users...."}</p>
+      <ul>
+        {users.map((user) => {
+          const { id, names } = user;
+          return (
+            <li
+              key={id}
+              data-id={id}
+              onClick={(e) => {
+                setRecipientId(e.currentTarget.dataset.id);
+              }}
+            >
+              {names}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default MessageSection;
